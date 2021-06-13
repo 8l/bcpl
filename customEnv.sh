4d10380c6a051b8245094847c2d4bb4d481c9c23
MatrixMike <matroxmike@icloud.com>
2021-06-14 09:56:00 +1000

    environment script

---
+++ customEnv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# was export BCPLROOT=$HOME/distribution/BCPL/cintcode
+export BCPLROOT=$HOME/BCPL/cintcode
+export BCPLPATH=$BCPLROOT/cin
+export BCPLHDRS=$BCPLROOT/g
+export BCPLSCRIPTS=$BCPLROOT/s
+export PATH=$PATH:$BCPLROOT/bin
+
+echo "$BCPLROOT"
+echo "$BCPLPATH"
+echo "$BCPLHDRS"
+echo "$BCPLSCRIPTS"
+
+
